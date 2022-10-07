ebf0c48284393c4bd0ae7efaf7ea174747a10e96
DINOEL <dinoel@MacBook-Air-DINOEL.local>
2022-10-07 22:06:36 +0300

    Tasl 8 completed

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -67,3 +67,22 @@ else
 }
 
 */
+
+// Задача 8:Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*
+
+Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{
+    int current = 2;
+    while (current <= n)
+    {
+        Console.Write(current + " ");
+        current += 2;    
+    }
+}
+
+*/
