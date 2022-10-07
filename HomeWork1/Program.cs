11d23b638b5d1dc22dd459ad158ca1240bf04c96
DINOEL <dinoel@MacBook-Air-DINOEL.local>
2022-10-07 20:49:04 +0300

    Task 2 completed

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input a fisrt number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine($"First number {n1} is greater then second number {n2}");
+}
+else if (n1 < n2)
+{
+    Console.WriteLine($"Second number {n2} is greater then fist number {n1}");
+}
+else
+{
+    Console.WriteLine($"First number {n1} and second number {n2} are equal.");
+}
